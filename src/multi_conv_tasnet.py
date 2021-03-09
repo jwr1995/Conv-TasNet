fixed normalization

src/multi_conv_tasnet.py
@@ -14,7 +14,7 @@ EPS = 1e-8
 
 class MultiConvTasNet(nn.Module):
     def __init__(self, N, L, B, H, P, X, R, C, norm_type="gLN", causal=False,
-                 mask_nonlinear='relu', n_channels=8):
+                 mask_nonlinear='relu', n_channels=8, figures=False):
         """
         Args:
             N: Number of filters in autoencoder
@@ -46,6 +46,8 @@ class MultiConvTasNet(nn.Module):
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_normal_(p)
+        self.figures = None
+        self.mask = None
 
     def forward(self, mixture):
         """
@@ -63,7 +65,13 @@ class MultiConvTasNet(nn.Module):
         T_origin = mixture.size(-1)
         T_conv = est_source.size(-1)
         est_source = F.pad(est_source, (0, T_origin - T_conv))
+
+        if self.figures:
+            self.mask=est_mask
+
         return est_source
+
+    def get_mask(self): return self.mask
 
     @classmethod
     def load_model(cls, path):

test/speedtest.py
@@ -1,21 +1,27 @@
 import time
 import os, sys
 import torch
+#from torchsummary import summary
+from pytorch_model_summary import summary
+
 sys.path.append('/home/will/Projects/Conv-TasNet/src')
 sys.path.append('/home/will/Projects/Conv-TasNet/ConferencingSpeech/baseline')
 
+from multi_conv_tasnet import MultiConvTasNet
+import utils
 
-from conv_tasnet import ConvTasNet
+utils.IS_CUDA=False
+
 N=256
-L=40
+L=60
 B=256
-H=512
+H=384
 P=3
 X=8
 R=4
 norm_type='gLN'
-causal=0
-mask_nonlinear='relu'
+causal=1
+mask_nonlinear='sigmoid'
 C=1
 # Training config
 use_cuda=1
@@ -35,16 +41,23 @@ momentum=0
 l2=0
 sample_rate=16000
 device = torch.device("cpu")
-model = ConvTasNet(N, L, B, H, P, X, R,
+model = MultiConvTasNet(N, L, B, H, P, X, R,
                    C, norm_type=norm_type, causal=causal,
                    mask_nonlinear=mask_nonlinear)
 model=model.eval()
 model.to(device)
-data = torch.rand([1,96000])
+data = torch.rand([1,8,96000])
 data=data.to(device)
 tic=time.perf_counter()
 output=model(data)
 toc=time.perf_counter()
 time=toc-tic
 seg_time = 96000/sample_rate
-print(output.shape,time)
+
+print("Time taken:",str(time),"s")
+print("RTF:",str(time/seg_time))
+
+utils.IS_CUDA=True
+model.cuda()
+
+print(summary(model, data.cuda()))
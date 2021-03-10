d53ee5d8520798eea070622dd22502019ad37a50
Will <will@Aithon>
2021-03-10 17:30:50 +0000

    eval_data_process

---
+++ egs/multi_cs210/prepare_eval.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Created on 2018/12
+# Author: Kaituo XU
+# Adapted 2021/02
+# Author: William Ravenscroft
+source /share/mini1/usr/will/miniconda3/bin/activate
+conda init
+conda activate cs21
+#export LDLIBRARYPATH=/share/mini1/sw/std/cuda/
+#_LIBRARY_PATH=${LD_LIBRARY_PATH}:/share/mini1/sw/std/cuda/cuda10.1/x86_64/lib64/:/share/mini1/sw/std/cuda/cuda10.1/x86_64/include/:/share/mini1/sw/std/cuda/cuda10.1/cuda/:/share/mini1/sw/std/cuda/cuda10.1/x86_64/lib64/stubs
+
+#data=/home/will/data/dummy/cs21
+data=/share/mini1/data/audvis/pub/se/mchan/mult/ConferencingSpeech/v1/eval_data/task1/
+stage=1  # Modify this to control to start from which stage
+
+dumpdir=data  # directory to put generated json file
+
+# -- START Conv-TasNet Config
+train_dir=$dumpdir/train
+valid_dir=$dumpdir/dev
+evaluate_dir=$dumpdir/eval
+separate_dir=$dumpdir/eval
+percentage=8
+sample_rate=16000
+segment=2  # seconds
+cv_maxlen=3   # seconds
+
+# Training config
+use_cuda=1
+id=0,1,2,3
+
+# minibatch
+shuffle=1
+batch_size=4
+num_workers=4
+# optimizer
+optimizer=adam
+lr=5e-3
+momentum=0
+l2=0
+# save and visualize
+checkpoint=1
+continue_from=""
+print_freq=10
+visdom=0
+visdom_epoch=0
+visdom_id="Conv-TasNet Training"
+# evaluate
+ev_use_cuda=1
+cal_sdr=1
+figures=True
+
+#corpus params
+corpus=cs21
+array=simu_non_uniform
+multichannel=True
+
+# -- END Conv-TasNet Config
+
+# exp tag
+tag="BIG" # tag for managing experiments.
+
+ngpu=1  # always 1
+
+. utils/parse_options.sh || exit 1;
+. ./cmd.sh
+. ./path.sh
+
+
+if [ $stage -le 1 ]; then
+  echo "Stage 1: Generating json files including wav path and duration"
+  [ ! -d $dumpdir ] && mkdir $dumpdir
+  preprocess.py --in-dir $data --out-dir $dumpdir --sample-rate $sample_rate --corpus $corpus --percentage $percentage
+fi
---
+++ src/preprocess_eval.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# Created on 2018/12
+# Author: Kaituo XU
+
+import argparse
+import json
+import os
+import random
+from itertools import  compress
+
+import soundfile as sf
+
+
+def preprocess_one_dir(in_dir, out_dir, out_filename, sample_rate=8000, entries=None, complete=False):
+    file_infos = []
+    in_dir = os.path.abspath(in_dir)
+    wav_list = os.listdir(in_dir)
+    if not entries == None:
+        wav_list = list(compress(wav_list,entries))
+    for wav_file in wav_list:
+        if not wav_file.endswith('.wav'):
+            continue
+        wav_path = os.path.join(in_dir, wav_file)
+        samples = sf.read(wav_path)[0]
+        if complete==True:
+            for channel in range(samples.shape[1]):
+                 # file path, num samples, num channels, channel idx
+                file_infos.append((wav_path, samples.shape[0],
+                samples.shape[1], channel))
+        else:
+            file_infos.append((wav_path, samples.shape[0],
+            samples.shape[1], 0))
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    with open(os.path.join(out_dir, out_filename + '.json'), 'w') as f:
+        json.dump(file_infos, f, indent=4)
+
+
+def preprocess(args):
+    for data_type in ['eval']:
+        num_files = len(os.listdir(os.path.abspath(os.path.join(args.in_dir, data_type, args.array, "mix"))))
+        entries = [bool(random.randrange(100) < args.percentage) for i in range(num_files)]
+        for source in ['real-recording', 'semi-real-playback','semi-real-realspk']:
+            preprocess_one_dir(os.path.join(args.in_dir, source,1),
+                               os.path.join(args.out_dir, data_type),
+                               source,
+                               sample_rate=args.sample_rate)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Corpus data preprocessing")
+    parser.add_argument('--in-dir', type=str, default=None,
+                        help='Directory path of corpus including train, dev and eval')
+    parser.add_argument('--out-dir', type=str, default=None,
+                        help='Directory path to put output files')
+    parser.add_argument('--sample-rate', type=int, default=8000,
+                        help='Sample rate of audio file')
+    parser.add_argument('--denoising', type=bool, default=True,
+                        help='Set Conv-TasNet to perform denoising')
+    parser.add_argument('--corpus', type=str, default="cs21",
+                        help='Set corpus')
+    parser.add_argument('--array', type=str, default='simu_non_uniform')
+    parser.add_argument('--percentage', type=float, default=20.0)
+    args = parser.parse_args()
+    print(args)
+    preprocess(args)
